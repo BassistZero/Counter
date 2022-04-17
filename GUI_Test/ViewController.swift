db0db4304ec314d2c3abc87f10d53fa0385fc17a
Bassist_Zero <bassist.zero@icloud.com>
2022-04-17 09:31:22 +0300

    MVP

--- GUI_Test/ViewController.swift
+++ GUI_Test/ViewController.swift
@@ -1,19 +1,34 @@
-//
-//  ViewController.swift
-//  GUI_Test
-//
-//  Created by Bassist_Zero on 4/12/22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var count = 0 {
+        didSet {
+            label.text = String(count)
+        }
     }
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var valueSlider: UISlider!
+
+    @IBAction func add(_ sender: UIButton) {
+        count += 1
+    }
+
+    @IBAction func reset(_ sender: UIButton) {
+        count = 0
+    }
+    
+    @IBAction func getValue(_ sender: UISlider) {
+        count = Int(valueSlider.value)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = String(count)
+        addButton.setTitle("Add", for: .normal)
+    }
 
 }
-
